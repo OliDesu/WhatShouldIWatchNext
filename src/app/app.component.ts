3ff447271fde75c4a82a3d4ffe747fd3dffb56e5
OliDesu <alielmufti25@gmail.com>
2024-08-19 08:44:05 +0200

    Set up wrapper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import OpenAI from "openai";
+import { EnvService } from '@ngx-env/core';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,37 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit {
+   openai = new OpenAI({ apiKey: process.env['OPENAI_API_KEY']
+     , dangerouslyAllowBrowser: true});
+
+  constructor(private envService: EnvService) {
+    this.openai = new OpenAI({ apiKey: this.envService.get('OPENAI_API_KEY') });
+  }
+
+
   title = 'WhatShouldIWatchNext';
+  response: String = '';
+  async ngOnInit(): Promise<void> {
+    const completion = await this.openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {role: "system", content: "You are a helpful assistant."},
+        {
+          role: "user",
+          content: "Write a haiku about recursion in programming.",
+        },
+      ],
+    });
+    this.response = String(completion.choices[0].message);
+    console.log(completion.choices[0].message);
+  }
 }
+
+
+
+
+
+
